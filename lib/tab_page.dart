b737fd669f4193ac758ff0e6a9ec25ee97385901
이총명 <dlchdaud1327@gmail.com>
2021-11-13 21:12:59 +0900

    [task] -add homepage -add tab paage

--- lib/tab_page.dart
+++ lib/tab_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clon/home_page.dart';
 
 class TabPage extends StatefulWidget {
   @override
@@ -6,10 +7,33 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _pages = [
+    HomePage(),
+    Text('page2'),
+    Text('page3'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('tab page')
+    return Scaffold(
+      body: Center(child : _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onItemTapped,
+        
+          currentIndex: _selectedIndex,
+          items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+        BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Search')),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Account')),
+      ]),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState((){
+      _selectedIndex = index;
+    });
   }
 }
